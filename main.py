3eb1e1d73130b915308abfc63695f0475bf7d502
Django <django@inventobot.com>
2020-03-13 10:36:57 -0700

    conecting

--- main.py
+++ main.py
@@ -1,4 +1,28 @@
 import tkinter
-import _pynetworktables
+import threading
 from pygame import midi
 midi.init()
+
+import threading
+from networktables import NetworkTables
+
+cond = threading.Condition()
+notified = [False]
+
+def connectionListener(connected, info):
+    print(info, '; Connected=%s' % connected)
+    with cond:
+        notified[0] = True
+        cond.notify()
+
+NetworkTables.initialize(server='127.0.0.1')
+NetworkTables.addConnectionListener(connectionListener, immediateNotify=True)
+
+with cond:
+    print("Waiting")
+    if not notified[0]:
+        cond.wait()
+
+table = NetworkTables.getTable("SmartDashboard")
+print(table.getKeys())
+
